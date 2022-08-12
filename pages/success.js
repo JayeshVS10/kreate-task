69b521ffaa8475eaa3701ab349899cce681d5493
Jayesh 10 <jayeshjvs51@gmail.com>
2022-08-12 13:48:44 +0530

    Commit

--- pages/success.js
+++ pages/success.js
@@ -23,11 +23,11 @@ const Success = () => {
           <BsBagCheckFill />
         </p>
         <h2>Thank you for your order!</h2>
-        <p className="email-msg">Check your email inbox for the receipt.</p>
+        <p className="email-msg">Order again</p>
         <p className="description">
           If you have any questions, please email
-          <a className="email" href="mailto:order@example.com">
-            order@example.com
+          <a className="email" href="mailto:jayeshjvs51@gmail.com">
+          jayeshjvs51@gmail.com
           </a>
         </p>
         <Link href="/">
